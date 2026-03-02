ae3287593c55ddf3c62b444e1b42191d6391de15
Camilo Parra <camiloandres0222@gmail.com>
2026-03-02 18:53:23 -0500

    ajustes

--- cajero electronico.py
+++ cajero electronico.py
@@ -6,29 +6,24 @@ def cajero_automatico():
 
 
     while True:
-        print("\n--- menu pricipal ---")
-        print("1. retiro rapido ")
+        print("--- menu pricipal ---")
+        print("1. consulta saldo ")
         print("2. retiro ")
-        print("3. consulta saldo ")
-        print("4. tranferencia" )
-        print("5. gestion de clave")
-        print("6. consulta movimientos ")
-        print("7. otras operaciones ")
+        print("3. retiro rapido ")
+        print("4. depositar ")
+        print("5. tranferencia" )
+        print("6. gestion de clave")
+        print("7. consulta movimientos ")
+        print("8. otras operaciones ")
         print("0. salir ")
 
         option = int(input("seleccione una opcion: "))
 
-        if option == 1:
-           monto = 100 
-           if saldo >= monto:
-              saldo -= monto
-              movimientos.append(f"retiro rapido: -${monto}")
-              print(f"retiro valido exitoso. Nuevo saldo: -${monto}")
+        if option == 1:  
+            print(f"saldo actual ${saldo}")
 
-           else:
-              print("fondos insuficientes. ")
-
-
+        
+        
         elif option == 2:
             try:
                 monto = float(input("ingrese monto retirar:" ))
@@ -41,26 +36,43 @@ def cajero_automatico():
                     movimientos.append(f"retiro: ${monto}")
                     print(f"retiro exitoso. Nuevo saldo ${saldo}")
             except ValueError:
-                print("ingrese un numeor valido. ")
+                print("ingrese un numero valido. ")
 
                                
 
         elif option == 3:
-            print(f"saldo actual: ${saldo}")
+        
+            clave_ingresada = input("ingrese su clave: ")
+            if clave_ingresada != clave:
+               print("clave correcta.")
 
+            else:
+                monto = 100 
+                if saldo >= monto:
+                  saldo -= monto
+                  movimientos.append(f"retiro rapido: ${monto}")
+                  print(f"retiro valido exitoso. Nuevo saldo: ${monto}")
+                else:
+                    print("fondos insuficientes. ")
+ 
+        
 
         elif option == 4:
             try:
-                cuenta_destino = int(input("ingrese numero cuenta de destino"))
-                monto = float(input("ingrese monto a transferir: "))
+                monto = float(input("ingrese monto a despositar: "))
                 if monto <= 0:
-                    print("fondos insuficientes. ")
-                else: 
-                    saldo -= monto 
-                    movimientos.append(f"transferencia a {cuenta_destino}: -${monto}")
-                    print(f"transferencia exitosa. Nuevo saldo: ${saldo}")
+                    print("monto invalido.")
+                else:
+                    saldo += monto
+                    movimientos.append(f"deposito: +${monto}")
+                    print(f"deposito exitoso. Nuevo saldo: $ {saldo}")
             except ValueError:
-                print("ingrese un numero valido. ")
+                print("ingrese un  numero valido")
+
+
+
+
+            
                         
 cajero_automatico()
 
